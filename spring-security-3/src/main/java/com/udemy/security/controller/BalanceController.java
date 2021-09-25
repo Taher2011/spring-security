3da79f6b245b8fcc9b76a142d962137f68e42b29
Taher2011 <taher.jit@gmail.com>
2021-09-25 09:03:26 +0530

    Spring security

---
+++ spring-security-3/src/main/java/com/udemy/security/controller/BalanceController.java
@@ -0,0 +1,14 @@
+package com.udemy.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+
+	@GetMapping("/balance")
+	public String getBalanceDetails() {
+		return "Balance details from DB";
+	}
+
+}
---
+++ spring-security-3/src/main/java/com/udemy/security/controller/ContactController.java
@@ -0,0 +1,14 @@
+package com.udemy.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+
+	@GetMapping("/contact")
+	public String saveContactDetails() {
+		return "Saving contact details into DB";
+	}
+
+}
