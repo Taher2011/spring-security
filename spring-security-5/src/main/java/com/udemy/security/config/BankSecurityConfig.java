f80c82edbd8a60e239aede6e7304b01cdaf4d560
Taher2011 <taher.jit@gmail.com>
2021-09-25 09:17:36 +0530

    Spring security

---
+++ spring-security-5/src/main/java/com/udemy/security/config/BankSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.udemy.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class BankSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	public void configure(HttpSecurity http) throws Exception {
+
+		/* Configuration to permit all request */
+
+		http.authorizeRequests().anyRequest().permitAll().and().formLogin().and().httpBasic();
+	}
+
+}
