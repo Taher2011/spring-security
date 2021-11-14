6fbf76ee50fc9f294cea5f34983b769bf7e0b8e5
Taher2011 <taher.jit@gmail.com>
2021-11-14 08:53:47 +0530

    Spring Sec

---
+++ spring-security-05/src/main/java/com/udemy/security/SpringSecurityBankApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBankApplication.class, args);
+	}
+
+}
