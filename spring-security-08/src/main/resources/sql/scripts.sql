daa54015170fab6fb4631e465cfc5892def30ea9
Taher2011 <taher.jit@gmail.com>
2021-11-14 11:35:23 +0530

    Add scripts

---
+++ spring-security-08/src/main/resources/sql/scripts.sql
@@ -0,0 +1,15 @@
+create database bank_spring_security;
+
+use bank_spring_security;
+
+CREATE TABLE bank_spring_security.users (
+id INT NOT NULL AUTO_INCREMENT,
+username VARCHAR(45) NOT NULL,
+password VARCHAR(45) NOT NULL,
+enabled INT NOT NULL,
+PRIMARY KEY (id));
+
+
+INSERT INTO bank_spring_security.users
+(id,username,password,enabled)
+VALUES(1,'bank-db','12345',1);
