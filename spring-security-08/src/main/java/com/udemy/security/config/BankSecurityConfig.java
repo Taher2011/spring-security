4241bea7519d5890c8591819409b838a6744ce44
Taher2011 <48172451+Taher2011@users.noreply.github.com>
2021-11-15 17:03:16 +0530

    Update BankSecurityConfig.java

--- spring-security-08/src/main/java/com/udemy/security/config/BankSecurityConfig.java
+++ spring-security-08/src/main/java/com/udemy/security/config/BankSecurityConfig.java
@@ -9,10 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
 public class BankSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
 
 	public void configure(HttpSecurity http) throws Exception {
 
@@ -21,9 +23,9 @@ public class BankSecurityConfig extends WebSecurityConfigurerAdapter {
 				.permitAll().antMatchers("/contact").permitAll().and().formLogin().and().httpBasic();
 	}
 
-	@Bean
-	public UserDetailsService userDetailsService(DataSource dataSource) {
-		return new JdbcUserDetailsManager(dataSource);
+	@Override
+	public void configure(AuthenticationManagerBuilder managerBuilder) throws Exception {
+		managerBuilder.jdbcAuthentication().dataSource(dataSource);
 	}
 
 	@Bean
